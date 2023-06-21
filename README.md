492b8384febb1c6f4c43350b244de53fb8b39980
Vladislav Tasev <vladislav.tasev@sap.com>
2023-06-21 15:13:17 +0300

    chore: initial

---
+++ README.md
@@ -0,0 +1,15 @@
+# Counts how many processes with a given name are running
+
+## Installation
+
+`yarn`
+
+## How to run
+
+`yarn start <SEARCH WORD>`
+
+f.e.
+
+`yarn start node`
+
+will tell you how many processing containing the word "node" are currently running
---
+++ index.js
@@ -0,0 +1,12 @@
+import psList from 'ps-list';
+import process from 'process';
+
+const search = process.argv[2] || "node";
+
+const run = async () => {
+	const list = await psList();
+	const count = list.filter(proc => proc.name.includes(search)).length;
+	console.log(`${count} processes containing "${search}" found`);
+}
+
+setInterval(run, 1000);
