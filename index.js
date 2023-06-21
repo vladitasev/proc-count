4f184c0ae29b1f55277d23d6fc965daadb4dd695
Vladislav Tasev <vladislav.tasev@sap.com>
2023-06-21 15:17:10 +0300

    chore: make case-insensitive

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const search = process.argv[2] || "node";
 
 const run = async () => {
 	const list = await psList();
-	const count = list.filter(proc => proc.name.includes(search)).length;
+	const count = list.filter(proc => proc.name.toLowerCase().includes(search.toLowerCase())).length;
 	console.log(`${count} processes containing "${search}" found`);
 }
 
